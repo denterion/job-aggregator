fbc854f87893acbb25228c397c6664b6d38fbfe3
AndrejeTokyo <centovandrej@gmail.com>
2026-05-01 11:22:25 +0300

    добавление новой функции

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"fmt"
 	"job-aggregator/internal/models"
 
 	"github.com/jackc/pgx/v5"
@@ -29,4 +30,30 @@ func (s *PostgresStorage) SaveVacancy(v models.Vacancy) error {
 
 	_, err := s.Conn.Exec(context.Background(), query, v.ID, v.Title, v.Description, v.Company, v.Location, v.Salary, v.URL, v.Source)
 	return err
+}
+
+func (s *PostgresStorage) GetLatestVacancies(limit int) ([]models.Vacancy, error) {
+	query := `
+		SELECT title, company, url, description, salary
+		FROM vacancies
+		ORDER BY created_at DESC
+		LIMIT $1
+	`
+	rows, err := s.Conn.Query(context.Background(), query, limit)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка подключения к бд: %v", err)
+	}
+
+	defer rows.Close()
+
+	var vacancies []models.Vacancy
+	for rows.Next() {
+		var v models.Vacancy
+		err := rows.Scan(&v.Title, &v.Company, &v.URL, &v.Description, &v.Salary)
+		if err != nil {
+			return nil, fmt.Errorf("ошибка сканирования строки: %v", err)
+		}
+		vacancies = append(vacancies, v)
+	}
+	return vacancies, nil
 }
